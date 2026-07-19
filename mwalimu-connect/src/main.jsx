c4b90aa0f2512c3867a78e3d14e2c65826302a7f
Lilian Muthoni <mainalilianm@gmail.comgit config --global --listnano ~/.gitconfiggit cofig user.namegit config --global user.email mainalilianm@gmail.com>
2026-07-19 21:11:05 +0300

    account balance basic output

--- mwalimu-connect/src/main.jsx
+++ mwalimu-connect/src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import { TutorEarnings } from './components/TutorEarnings.jsx'
+import { AccountBalance } from './components/AccountBalance.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <TutorEarnings />
   </StrictMode>,
 )
