24b8ae47a0ae45a8551f4b3b7c0662e3cecae440
Lilian Muthoni <mainalilianm@gmail.comgit config --global --listnano ~/.gitconfiggit cofig user.namegit config --global user.email mainalilianm@gmail.com>
2026-08-04 20:52:28 +0300

    admin dashboard

--- mwalimu-connect/src/App.jsx
+++ mwalimu-connect/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { TutorEarnings } from "./components/TutorEarnings"
 import { AccountBalance } from "./components/AccountBalance"
+import { AdminDashboard } from "./components/AdminDashboard"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/accountbalance" element={<AccountBalance />} />
         <Route path="/tutorearnings" element={<TutorEarnings />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
       </Routes>  
     </BrowserRouter>
   )
