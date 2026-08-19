80d2d0b644dc60cc10e0aa6d1fff05f46fd1e25f
Ronaldo Nyakwama <nyabayonyakwama@outlook.com>
2026-08-20 00:49:21 +0300

    Remove unused Button import from AccountBalance

--- mwalimu-connect/src/pages/AccountBalance.jsx
+++ mwalimu-connect/src/pages/AccountBalance.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { Button } from './Button';
+import { Button } from '../components/Button';
 import {DollarSign, ArrowUpRight, ArrowDownRight} from 'lucide-react'
 
 export function AccountBalance(){
